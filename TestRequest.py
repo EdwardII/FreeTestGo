d1d65a3fd4ce0a31d9b2441bee2481e57fbf1ed5
LangJin <fenyukuang@163.com>
2017-04-07 14:54:32 +0800

    增加判断的多样性

--- TestRequest.py
+++ TestRequest.py
@@ -13,19 +13,34 @@ def TestPostRequest(hurl,hdata,headers,htestcassid,htestcassname,htesthope):
     hr = requests.post(hurl,data=json.dumps(hdata), headers=headers)
     hjson = json.loads(hr.text)#获取并处理返回的json数据
     herror ="error"
-    if herror in hjson:       
-        hhhdata = {"t_id": htestcassid,
+    if herror in hjson: 
+        hstatus = str(hjson["status"])
+        if hstatus == htesthope:
+            hhhdata = {"t_id": htestcassid,
                           "t_name": htestcassname,
                           "t_method": "post", 
                           "t_url": hurl, 
                           "t_param": "测试数据:"+str(hdata),
-                          "t_hope": "Code:"+str(htesthope), 
-                          "t_actual": str(hjson), 
-                          "t_result": "失败"} 
-        hlist.append(hhhdata)
-        print(htestcassname)
-        print('测试不通过')
-        print("返回的消息为："+str(hjson))
+                          "t_hope": "code:"+str(htesthope), 
+                          "t_actual": "Code:"+hstatus+";msg:"+str(hjson['message']), 
+                          "t_result": "通过"} 
+            hlist.append(hhhdata)#把测试结果添加到数组里面
+            print(htestcassname)
+            print("测试通过")
+            print("返回的消息是："+str(hjson['message']))  
+        else:          
+            hhhdata = {"t_id": htestcassid,
+                              "t_name": htestcassname,
+                              "t_method": "post", 
+                              "t_url": hurl, 
+                              "t_param": "测试数据:"+str(hdata),
+                              "t_hope": "Code:"+str(htesthope), 
+                              "t_actual": str(hjson), 
+                              "t_result": "失败"} 
+            hlist.append(hhhdata)
+            print(htestcassname)
+            print('测试不通过')
+            print("返回的消息为："+str(hjson))
     else:
         hcode = str(hjson['code'])     
         if hcode == htesthope:  
@@ -56,22 +71,37 @@ def TestPostRequest(hurl,hdata,headers,htestcassid,htestcassname,htesthope):
             print("返回的消息是："+str(hjson['msg']))
 
 def TestGetRequest(hurl,hdata,headers,htestcassid,htestcassname,htesthope):
-    hr = requests.get(hurl,params=json.dumps(hdata), headers=headers)
+    hr = requests.get(hurl,params=hdata, headers=headers)
     hjson = json.loads(hr.text)#获取并处理返回的json数据
     herror ="error"
     if herror in hjson:       
-        hhhdata = {"t_id": htestcassid,
+        hstatus = str(hjson["status"])
+        if hstatus == htesthope:
+            hhhdata = {"t_id": htestcassid,
                           "t_name": htestcassname,
                           "t_method": "post", 
                           "t_url": hurl, 
                           "t_param": "测试数据:"+str(hdata),
                           "t_hope": "code:"+str(htesthope), 
-                          "t_actual": str(hjson), 
-                          "t_result": "失败"} 
-        hlist.append(hhhdata)
-        print(htestcassname)
-        print('测试不通过')
-        print("返回的消息为："+str(hjson))
+                          "t_actual": "Code:"+hstatus+";msg:"+str(hjson['errors']), 
+                          "t_result": "通过"} 
+            hlist.append(hhhdata)#把测试结果添加到数组里面
+            print(htestcassname)
+            print("测试通过")
+            print("返回的消息是："+str(hjson['errors']))  
+        else:            
+            hhhdata = {"t_id": htestcassid,
+                              "t_name": htestcassname,
+                              "t_method": "post", 
+                              "t_url": hurl, 
+                              "t_param": "测试数据:"+str(hdata),
+                              "t_hope": "code:"+str(htesthope), 
+                              "t_actual": str(hjson), 
+                              "t_result": "失败"} 
+            hlist.append(hhhdata)
+            print(htestcassname)
+            print('测试不通过')
+            print("返回的消息为："+str(hjson))
     else:
         hcode = str(hjson['code'])     
         if hcode == htesthope:  
