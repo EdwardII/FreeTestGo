bb70f3b2fd52a6f607c994743bd03b1c5877935b
LangJin <fenyukuang@163.com>
2017-04-11 09:51:53 +0800

    测试用例1
    请根据自己的项目情况编写

--- TestCass/AdminTestCass_01.py
+++ TestCass/AdminTestCass_01.py
@@ -1,150 +1,126 @@
 # -*- coding:utf-8 -*-  
 
 '''
-Created on 2017年3月29日
+Created on 2017年4月6日
 
 @author: Jin
 '''
-import json
-import requests
 import xlrd
-
-Testdata = xlrd.open_workbook('C:/Jin/workpase/ApiTest/src/TestData.xls')#读取测试数据
+import PublicTools.TestRequest
+from PublicTools.TestRequest import TestPostRequest
+from PublicTools.TestRequest import TestGetRequest
+hhlist =PublicTools.TestRequest.hlist
+Testdata = xlrd.open_workbook('C:/Jin/workpase/ApiTest/src/AdminTest/TestData/TestData.xls')#读取测试数据
 table = Testdata.sheets()[0]#选择excle表中的sheet
 hurl = table.cell(7,1).value#从测试数据中读取url
 htoken = table.cell(8,1).value
 hcontent_type = table.cell(6,1).value
-hlist = []#添加一个数组，用来装测试结果
 def test_1add_admin():
     for i in range(3,9):
-        table = Testdata.sheets()[1]#选择excle表中的sheet
-        username = table.cell(i,0).value#读取测试数据
-        realName = table.cell(i,1).value
-        status = table.cell(i,2).value
-        password = table.cell(i,3).value
-        remark = table.cell(i,4).value        
-        data = {
-                "username": username,
-                "realName": realName,
-                "password": password,
-                "remark": remark,
-                "status":status
-                }
-             
+        table = Testdata.sheets()[1]#选择excle表中的sheet  
+        hdata = {
+                "username": table.cell(i,0).value,
+                "realName": table.cell(i,1).value,
+                "password": table.cell(i,3).value,
+                "remark": table.cell(i,4).value,
+                "status":table.cell(i,2).value
+                }             
         headers = {
             'content-type': hcontent_type,
             'token': htoken
             }
-        r = requests.post(hurl+'/admin/add', data=json.dumps(data), headers=headers)
-        hjson = json.loads(r.text)#获取并处理返回的json数据
-        hstr = "error"
-        if hstr in hjson:
-#             print(hjson)
-            hhhdata = {"t_id": '1-'+str(i+1), 
-                          "t_name": "新增管理接口", 
-                          "t_method": "post", 
-                          "t_url": hurl+'/admin/add', 
-                          "t_param": "username:"+str(username)+",realName:" +str(realName)+",password:"+str(password)+",remark:"+str(remark)+",status:"+str(status),
-                          "t_hope": "{code:200,msg:登陆成功}", 
-                          "t_actual": str(hjson), 
-                          "t_result": "失败"} 
-            hlist.append(hhhdata)
-            print("添加管理员")
-            print('测试不测试通过')
-            print("返回的消息为："+str(hjson))
-        else:
-            hcode = str(hjson['code'])     
-            if hcode == table.cell(i,5).value:
-                hmsg = str(hjson['msg'])    
-                hhhdata = {"t_id": '1-'+str(i+1), 
-                          "t_name": "新增管理接口", 
-                          "t_method": "post", 
-                          "t_url": hurl+'/admin/add', 
-                          "t_param": "username:"+str(username)+",realName:" +str(realName)+",password:"+str(password)+",remark:"+str(remark)+",status:"+str(status),
-                          "t_hope": "{code:200,msg:新增管理接口}", 
-                          "t_actual": 'code:'+str(hcode)+','+'msg:'+str(hmsg), 
-                          "t_result": "通过"}    
-                hlist.append(hhhdata)#把测试结果添加到数组里面
-                print("添加管理员")
-                print('测试通过')
-                print('请求返回消息为：'+str(hjson['msg']))   
-            else:
-                hmsg = str(hjson['msg']) 
-                hhhdata = {"t_id": '1-'+str(i+1), 
-                          "t_name": "新增管理接口", 
-                          "t_method": "post", 
-                          "t_url": hurl+'/admin/add', 
-                          "t_param": "username:"+str(username)+",realName:" +str(realName)+",password:"+str(password)+",remark:"+str(remark)+",status:"+str(status),
-                          "t_hope": "{code:200,msg:新增管理接口}", 
-                          "t_actual": 'code:'+str(hcode)+','+'msg:'+str(hmsg), 
-                          "t_result": "失败"} 
-                hlist.append(hhhdata)
-                print("添加管理员")
-                print('测试不通过')
-                print('请求返回状态为：'+hcode)
-                print('请求返回消息为：'+str(hjson['msg']))
-                print('---------------------------')
-
+        htestcassid = "1-1-"+str(i+1)
+        htestcassname = "添加管理员"
+        htesthope = table.cell(i,5).value
+        TestPostRequest(hurl+'/admin/add', hdata, headers, htestcassid, htestcassname,htesthope)
+        
 def test_2del_admin():
     for i in range(13,16):
-        table = Testdata.sheets()[1]#选择excle表中的sheet
-        hid = table.cell(i,0).value     
+        table = Testdata.sheets()[1]#选择excle表中的sheet  
+        hid = table.cell(i,0).value    
+        hdata = None             
         headers = {
             'content-type': hcontent_type,
             'token': htoken
             }
-        r = requests.post(hurl+'/admin/delete?id='+hid, headers=headers)
-        hjson = json.loads(r.text)#获取并处理返回的json数据
-        hstr = "error"
-        if hstr in hjson:
-#             print(hjson)
-            hhhdata = {"t_id": '2-'+str(i+1), 
-                          "t_name": "删除管理员", 
-                          "t_method": "post", 
-                          "t_url": hurl+'/admin/delete?',
-                          "t_param": "id:"+hid,
-                          "t_hope": "{code:200,msg:删除成功}", 
-                          "t_actual": str(hjson), 
-                          "t_result": "失败"} 
-            hlist.append(hhhdata)
-            print("删除管理员")
-            print('测试不通过')
-            print("返回的消息为："+str(hjson))
-        else:
-            hcode = str(hjson['code'])     
-            if hcode == table.cell(i,5).value:
-                hmsg = str(hjson['msg'])    
-                hhhdata = {"t_id": '2-'+str(i+1), 
-                          "t_name": "删除管理员", 
-                          "t_method": "post", 
-                          "t_url": hurl+'/admin/delete?',
-                          "t_param": "id:"+hid,
-                          "t_hope": "{code:200,msg:删除成功}", 
-                          "t_actual": 'code:'+str(hcode)+','+'msg:'+str(hmsg), 
-                          "t_result": "通过"}    
-                hlist.append(hhhdata)#把测试结果添加到数组里面
-                print("删除管理员")
-                print('测试通过')
-                print('请求返回消息为：'+str(hjson['msg']))   
-            else:
-                hmsg = str(hjson['msg']) 
-                hhhdata = {"t_id": '2-'+str(i+1), 
-                          "t_name": "删除管理员", 
-                          "t_method": "post", 
-                          "t_url": hurl+'/admin/delete?',
-                          "t_param": "id:"+hid,
-                          "t_hope": "{code:200,msg:新增管理接口}", 
-                          "t_actual": 'code:'+str(hcode)+','+'msg:'+str(hmsg), 
-                          "t_result": "失败"} 
-                hlist.append(hhhdata)
-                print("删除管理员")
-                print('测试不通过')
-                print('请求返回状态为：'+hcode)
-                print('请求返回消息为：'+str(hjson['msg']))
-                print('---------------------------')
+        htestcassid = "1-2-"+str(i+1)
+        htestcassname = "删除管理员"
+        htesthope = table.cell(i,1).value
+        TestPostRequest(hurl+'/admin/delete?id='+hid, hdata, headers, htestcassid, htestcassname,htesthope)       
 
+def test_3edit_admin():
+    for i in range(21,25):
+        table = Testdata.sheets()[1]#选择excle表中的sheet  
+         
+        hdata = {
+                "id": table.cell(i,0).value,
+                "realName": table.cell(i,1).value,
+                "adminType": table.cell(i,2).value,
+                "remark": table.cell(i,3).value,
+                "status":table.cell(i,4).value            
+            }           
+        headers = {
+            'content-type': hcontent_type,
+            'token': htoken
+            }
+        htestcassid = "1-3-"+str(i+1)
+        htestcassname = "修改管理员"
+        htesthope = table.cell(i,5).value
+        TestPostRequest(hurl+'/admin/edit', hdata, headers, htestcassid, htestcassname,htesthope)       
 
+def test_4list_admin():
+    for i in range(31,35):
+        table = Testdata.sheets()[1]#选择excle表中的sheet  
+         
+        hdata = {
+            "status":table.cell(i,0).value,
+            "username":table.cell(i,1).value         
+            }           
+        headers = {
+            'content-type': hcontent_type,
+            'token': htoken
+            }
+        htestcassid = "1-4-"+str(i+1)
+        htestcassname = "返回管理员列表"
+        htesthope = table.cell(i,2).value
+        TestGetRequest(hurl+"/admin/getByStatusAndUsername", hdata, headers, htestcassid, htestcassname, htesthope)
+
+def test_5exists_admin():  
+    for i in range(40,42):
+        table = Testdata.sheets()[1]#选择excle表中的sheet  
+         
+        hdata = {
+            "username":table.cell(i,0).value         
+            }           
+        headers = {
+            'content-type': hcontent_type,
+            'token': htoken
+            }
+        htestcassid = "1-5-"+str(i+1)
+        htestcassname = "判断管理员姓名是否已存在"
+        htesthope = table.cell(i,1).value
+        TestGetRequest(hurl+"/admin/existsByUserName", hdata, headers, htestcassid, htestcassname, htesthope)  
+def test_6find_admin():
+    for i in range(48,52):
+        table = Testdata.sheets()[1]#选择excle表中的sheet  
+         
+        hdata = {
+                "pageNo": table.cell(i,0).value,
+                "pageSize": table.cell(i,1).value,   
+            }           
+        headers = {
+            'content-type': hcontent_type,
+            'token': htoken
+            }
+        htestcassid = "1-6-"+str(i+1)
+        htestcassname = "查询管理员分页列表"
+        htesthope = table.cell(i,2).value
+        TestPostRequest(hurl+'/admin/findByCondition', hdata, headers, htestcassid, htestcassname,htesthope)   
+        
 # test_1add_admin()
-# test_2del_admin()
-   
-                
+# test_2dle_admin()
+# test_3edit_admin()
+# test_4list_admin()
+# test_5exists_admin()
+# test_6find_admin()
+
