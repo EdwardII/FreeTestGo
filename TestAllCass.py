8bff782f9dc5000b19712e0845d753d83b801557
LangJin <fenyukuang@163.com>
2017-04-11 09:55:59 +0800

    构建测试集

---
+++ TestAllCass.py
@@ -0,0 +1,58 @@
+# -*- coding:utf-8 -*-  
+
+'''
+Created on 2017年3月29日
+
+@author: Jin
+
+'''
+#导入测试用例
+import time
+import TestCass.TestCassAdmin_01
+import TestCass.TestCassPermission_02
+import TestCass.TestCassAdminrole_03
+
+
+hhhlist = []
+def Test_01AdminCass():
+    print('开始测试')
+    now = time.strftime("%Y-%m-%d-%H-%M-%S",time.localtime(time.time()))
+    print(now)
+    TestCass.TestCassAdmin_01.test_1add_admin()#添加需要测试测试用例
+    TestCass.TestCassAdmin_01.test_2del_admin()
+    TestCass.TestCassAdmin_01.test_3edit_admin()
+    TestCass.TestCassAdmin_01.test_4list_admin()
+    TestCass.TestCassAdmin_01.test_5exists_admin()
+    TestCass.TestCassAdmin_01.test_6find_admin()
+    TestReportData = TestCass.TestCassAdmin_01.hhlist#返回测试结果。
+#     print(TestReportData)
+    print('结束测试')
+    hhhlist.extend(TestReportData) 
+    
+def Test_02PermissionCass():
+    print("开始测试")
+    now = time.strftime("%Y-%m-%d-%H-%M-%S",time.localtime(time.time()))
+    print(now)
+    TestCass.TestCassPermission_02.test_1add_permission()
+    TestCass.TestCassPermission_02.test_2del_permission()
+    TestCass.TestCassPermission_02.test_3find_permission()
+    TestCass.TestCassPermission_02.test_4exists_permission()
+    TestReportData = TestCass.TestCassPermission_02.hhlist
+    print('结束测试')
+    hhhlist.extend(TestReportData) 
+
+def Test_03AdminroleCass():
+    print("开始测试")
+    now = time.strftime("%Y-%m-%d-%H-%M-%S",time.localtime(time.time()))
+    print(now)
+    TestCass.TestCassAdminrole_03.test_1relate_permissions()
+    TestCass.TestCassAdminrole_03.test_2get_permissions()
+    TestReportData = TestCass.TestCassAdminrole_03.hhlist
+    print("结束测试")  
+    hhhlist.extend(TestReportData)  
+
+
+ 
+# Test_01AdminCass()    
+# Test_02PermissionCass()
+# Test_03AdminroleCass()
